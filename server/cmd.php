59c15ba7993f728345f3e0692c2dbe5318c3f3bc
Aaeon Ogle <aaron@geekgonecrazy.com>
2014-04-05 00:42:54 -0500

    removing old code

--- server/cmd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$command = $_GET['p'];
-$host = getenv(REMOTE_ADDR);
-$output = shell_exec('python /var/www/Serverpush.py ' . $host . ' '. $command);
-echo $output;
-
-Header( 'Location: https://10.10.10.48' ) ;
-
-?>
